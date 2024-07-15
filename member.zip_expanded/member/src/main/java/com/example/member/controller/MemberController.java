5eb29a79a799519f55bc38c2782b531cf6d1023c
oofy <oofysotry@gmail.com>
2024-07-15 16:40:40 +0900

    first commit

---
+++ member.zip_expanded/member/src/main/java/com/example/member/controller/MemberController.java
@@ -0,0 +1,33 @@
+package com.example.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.member.dto.MemberDTO;
+import com.example.member.service.MemberService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService = new MemberService();
+
+    // 회원가입 페이지 출력 요청
+    @GetMapping("/member/save")
+    public String saveForm() {
+        return "save";
+    }
+
+    @PostMapping("/member/save")    // name값을 requestparam에 담아온다
+    public String save(@ModelAttribute MemberDTO memberDTO) {
+        System.out.println("MemberController.save");
+        System.out.println("memberDTO = " + memberDTO);
+        memberService.save(memberDTO);
+
+        return "index";
+    }
+}
